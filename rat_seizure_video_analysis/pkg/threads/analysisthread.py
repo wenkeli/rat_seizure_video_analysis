632320713cccbc6ff3288c1f1cf66a26603a82b5
wenke.li <wenke.li@utexas.edu>
2015-06-02 16:19:37 -0500

    bugfix

--- rat_seizure_video_analysis/pkg/threads/analysisthread.py
+++ rat_seizure_video_analysis/pkg/threads/analysisthread.py
@@ -22,25 +22,24 @@ class AnalysisThread(QThread):
         numVids=0;
         
         while(True):
-            if(self.__tData.analysisInd%self.__tData.framesPerVid==0):
-                if(vidOut is not None):
-                    vidOut.release();
-                
-                timestamp=time.time();
-                dt=datetime.fromtimestamp(timestamp);
-                
-                fName=(str(dt.year)+"_"+str(dt.month)+"_"+str(dt.day)+"_"+
-                       self.__tData.ratIDs[0]+"_"+str(dt.hour)+"_"+str(dt.minute)+".mov");
-                fName=os.path.join(self.__tData.ratDirs[0],
-                                   fName);
-                vidOut=cv2.VideoWriter(fName, fourcc, 30, (320,240));
-                if(not vidOut.isOpened()):
-                    print("can't open video writer! >:(");
-                    break;
-                print("video #"+str(numVids));
-                numVids=numVids+1;
-            
             if(self.__tData.analysisInd<self.__tData.acqFInd):
+                if(self.__tData.analysisInd%self.__tData.framesPerVid==0):
+                    if(vidOut is not None):
+                        vidOut.release();
+                    
+                    timestamp=time.time();
+                    dt=datetime.fromtimestamp(timestamp);
+                    
+                    fName=(str(dt.year)+"_"+str(dt.month)+"_"+str(dt.day)+"_"+
+                           self.__tData.ratIDs[0]+"_"+str(dt.hour)+"_"+str(dt.minute)+".mov");
+                    fName=os.path.join(self.__tData.ratDirs[0],
+                                       fName);
+                    vidOut=cv2.VideoWriter(fName, fourcc, 30, (320,240));
+                    if(not vidOut.isOpened()):
+                        print("can't open video writer! >:(");
+                        break;
+                    print("video #"+str(numVids));
+                    numVids=numVids+1;
                 
                 curFrame=self.__tData.vidFrames[self.__tData.analysisInd];
                 self.__tData.vidFrames[self.__tData.analysisInd]=None;
