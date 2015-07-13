e61f814c2a5cb7557aa289ff90c8f58601defb29
consciousness <wenke.li@utexas.edu>
2015-07-13 13:01:11 -0500

    testing the memory consumption of a simple acquisition program

--- rat_seizure_video_analysis/testacquire.py
+++ rat_seizure_video_analysis/testacquire.py
@@ -6,22 +6,39 @@ from pkg.data.videorecord import VideoRecord;
 
 
 rec=VideoRecord("test", "/Users/consciousness/Desktop/testvids/test", 1, 1800);
-acq=VideoAcquire(0);
-acq.initCamera();
+# acq=VideoAcquire(0);
+# acq.initCamera();
 
-# cam=cv2.VideoCapture(0);
-# print(cam.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320));
-# print(cam.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240));
-# print(cam.set(cv.CV_CAP_PROP_FPS, 30));
-# print(cam.set(cv.CV_CAP_PROP_BRIGHTNESS, 150));
-# print(cam.set(cv.CV_CAP_PROP_EXPOSURE, 110));
+cam=cv2.VideoCapture(0);
+print(cam.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320));
+print(cam.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240));
+print(cam.set(cv.CV_CAP_PROP_FPS, 30));
+print(cam.set(cv.CV_CAP_PROP_BRIGHTNESS, 150));
+print(cam.set(cv.CV_CAP_PROP_EXPOSURE, 110));
 
+# cam=cv.CaptureFromCAM(0);
 
-for i in np.r_[0:1800]:
-    frame=acq.acquireFrame();
-    rec.writeNextFrame(frame);
-    
-#     (success, frame)=cam.read();
+i=0;
+
+# frame=np.zeros((240, 320, 3), dtype="uint8");
+
+while(True):
+# for i in np.r_[0:7200]:
+#     frame=acq.acquireFrame();
+#     rec.writeNextFrame(frame);
+    if(i>=7200):
+        break;
+    (success, frame)=cam.read();
+#     del(frame);
+#     if(not cam.grab()):
+#     if(not cv.GrabFrame(cam)):
+#         continue;
+#     cam.retrieve(frame);
+#     cam.retrieve();
+#     cv.RetrieveFrame(cam);
+#     del(frame1);
+#     del(success);
+    i=i+1;
 
     if((i%100)==0):
         print(str(i));
